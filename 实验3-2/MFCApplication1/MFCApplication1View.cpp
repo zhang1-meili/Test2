9299b5d4857e77729d520f484d3c3f059b045a2d
zhang1-meili <1031601344@qq.com>
2020-04-06 20:39:08 +0800

    2020年4月6号上传实验三

---
+++ 实验3-2/MFCApplication1/MFCApplication1View.cpp
@@ -0,0 +1,98 @@
+
+// MFCApplication1View.cpp : CMFCApplication1View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MFCApplication1.h"
+#endif
+
+#include "MFCApplication1Doc.h"
+#include "MFCApplication1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication1View
+
+IMPLEMENT_DYNCREATE(CMFCApplication1View, CView)
+
+BEGIN_MESSAGE_MAP(CMFCApplication1View, CView)
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CMFCApplication1View 构造/析构
+
+CMFCApplication1View::CMFCApplication1View()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CMFCApplication1View::~CMFCApplication1View()
+{
+}
+
+BOOL CMFCApplication1View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMFCApplication1View 绘制
+
+void CMFCApplication1View::OnDraw(CDC* pDC)
+{
+	CMFCApplication1Doc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+	for (int i = 0; i < ca.GetSize(); i++)
+
+		pDC->Ellipse(ca.GetAt(i));
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+
+// CMFCApplication1View 诊断
+
+#ifdef _DEBUG
+void CMFCApplication1View::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMFCApplication1View::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMFCApplication1Doc* CMFCApplication1View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMFCApplication1Doc)));
+	return (CMFCApplication1Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMFCApplication1View 消息处理程序
+
+
+void CMFCApplication1View::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	CRect cr;
+	int r1 = rand() % 50 + 5;
+	int r2 = rand() % 50 + 5;
+	cr.SetRect(point.x - r1, point.y + r2, point.x + r1, point.y - r2);
+	ca.Add(cr);
+	this->Invalidate();
+	CView::OnLButtonDown(nFlags, point);
+}
---
+++ 实验3-2/MFCApplication1/MFCApplication1View.h
@@ -0,0 +1,48 @@
+
+// MFCApplication1View.h : CMFCApplication1View 类的接口
+//
+
+#pragma once
+
+
+class CMFCApplication1View : public CView
+{
+protected: // 仅从序列化创建
+	CMFCApplication1View();
+	DECLARE_DYNCREATE(CMFCApplication1View)
+
+// 特性
+public:
+	CMFCApplication1Doc* GetDocument() const;
+
+// 操作
+public:
+	CArray<CRect, CRect&>  ca;
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CMFCApplication1View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // MFCApplication1View.cpp 中的调试版本
+inline CMFCApplication1Doc* CMFCApplication1View::GetDocument() const
+   { return reinterpret_cast<CMFCApplication1Doc*>(m_pDocument); }
+#endif
+
